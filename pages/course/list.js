b51569dc8ff1e6cfe9ff1c2cb1fff904b242dc3f
Antolina Gonzalez <antolina487@gmail.com>
2020-10-09 20:20:28 -0300

    token:

--- pages/course/list.js
+++ pages/course/list.js
@@ -15,17 +15,23 @@ import NavBar from "../../components/navbar";
 import Layout from "../../components/layout";
 import styles from "../../styles/Home.module.css";
 import React, { useState } from "react";
+import nextCookie from 'next-cookies';
+import Cookies from 'js-cookie';
 
 Courses.getInitialProps = async () => {
-  
+  const token = await axios.post(`${process.env.API_BASE_URL}/users/login`, {
+    email: 'antolina487@gmail.com',
+    password:'jejej4'
+  })
+  console.log(token.data.accessToken)
   const courses = await axios.get(`${process.env.API_BASE_URL}/courses`, {
     headers: {
       Authorization: `Bearer ` + axios.defaults.headers.common.Authorization,
     },
   });
-  console.log(courses.config.headers.Authorization)
+  //console.log(courses.config.headers.Authorization)
   return {
-    header:courses.config.headers.Authorization,
+    data: token.data.accessToken,
     courses: courses.data.courses,
     user: courses.data.user,
   };
@@ -39,7 +45,6 @@ export default function Courses(props) {
   const [alert, setAlert] = useState(false);
   const handleCloseAlert = () => setAlert(false);
   const handleShowAlert = () => setAlert(true);
-  console.log(props)
   if (props.user == "noUser") {
     return (
       <div className={styles.container}>
@@ -60,7 +65,7 @@ export default function Courses(props) {
             <Jumbotron className="bg-white">
               <h1>No estas logeado</h1>
               <h3>{props.user}</h3>
-              <h3>{props.header}</h3>
+              <h3>{props.data}</h3>
               <p>
                 <Button
                   variant="success"
@@ -95,7 +100,7 @@ export default function Courses(props) {
             <Row className="justify-content">
               <Col>
                 <h5>{props.user}</h5>
-                <h3>{props.header}</h3>
+                <h3>{props.data}</h3>
               </Col>
             </Row>
             <p>
