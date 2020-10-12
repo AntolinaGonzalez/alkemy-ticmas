ae5481a023db7cadf8e839f04dcbf0073688a6e2
Antolina Gonzalez <antolina487@gmail.com>
2020-10-11 23:11:14 -0300

    feature: set token temp solution

--- pages/api/login.js
+++ pages/api/login.js
@@ -1,20 +1,23 @@
 import axios from "axios";
+import { setToken } from "./Helpers/auth-Helpers";
 
-export default (req, res) => {
+export default async (req, res) => {
   const body = req.query;
-  axios({
-    method: "post",
-    url: `${process.env.API_BASE_URL}/users/login`,
-    data: body,
-  })
-    .then(function (response) {
-      axios.defaults.headers.common["Authorization"] =
-        response.data.accessToken;
-        res.redirect("../course/list");
-    })
-    .catch(function (error) {
-      if (error.response.status === 401) {
-        res.redirect("../error");
-      }
-    });
+  const { data } = await axios.post(`${process.env.API_BASE_URL}/users/login`, {
+    email: body.email,
+    password: body.password,
+  });
+  setToken(data.accessToken)
+  res.redirect("../course/list")
+  // .then(function (response) {
+  //   //setToken(response.data.accessToken)
+  //   axios.defaults.headers.common["Authorization"] =
+  //     response.data.accessToken;
+  //     res.redirect("../course/list");
+  // })
+  // .catch(function (error) {
+  //   if (error.response.status === 401) {
+  //     res.redirect("../error");
+  //   }
+  // });
 };
